afec68caa24db89ea7a31ddbbc6512a3bf688d7e
daszko09 <86254334+daszko09@users.noreply.github.com>
2022-02-16 17:50:45 +0100

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,3 +1,9 @@
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+# 1. Add on to the previous program by asking the user for another number and printing out that many 
+# copies of the previous message.
+# 2. Print out that many copies of the previous message on separate lines.
+
 import datetime
 teraz=datetime.datetime.now()
 
